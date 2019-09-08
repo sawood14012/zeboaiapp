c854097667475beee7965f59652e48409c57eaad
Jugal <jugaldeepak@gmail.com>
2019-09-08 18:26:01 +0530

    update chart page

--- lib/pages/chart_page.dart
+++ lib/pages/chart_page.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class ChartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Chart',
+        ),
+      ),
+      body: Container(),
+    );
   }
 }
